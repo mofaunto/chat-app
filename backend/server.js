ea005f521e9ee940ef20be25fd06cb6a12f0bc8b
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-02-01 14:29:00 +0900

    Set up User Schema, preparing for authentication

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
-dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
+const app = express();
+
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is on");
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
