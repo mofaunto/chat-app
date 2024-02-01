74612b0ac519152586895ac49d243bd93917af28
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-02-01 13:00:29 +0900

    Initializing the project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is on");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
