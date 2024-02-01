e667dc75e71ee2657ae741a1d6d57a28c79aa325
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-02-01 17:56:31 +0900

    Added message and user models and controllers, initial backend is ready

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,8 +14,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //needed to parse incoming requests with JSON payload coming from req.body
+app.use(cookieParser()); //needed to parse incoming cookies from req.cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Server is on");
