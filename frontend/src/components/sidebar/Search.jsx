c441a0d0ab9c291f926f1fa4345c638f48a5a525
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-02-02 19:54:53 +0900

    Added search function

--- frontend/src/components/sidebar/Search.jsx
+++ frontend/src/components/sidebar/Search.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustandstore/useConversation";
+import useGetChats from "../../hooks/useGetChats";
+import toast from "react-hot-toast";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { chats } = useGetChats();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 characters long");
+    }
+
+    const chat = chats.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (chat) {
+      setSelectedConversation(chat);
+      setSearch("");
+    } else {
+      toast.error("No such user found");
+    }
+  };
+
   return (
-    <form className='flex items-center gap-2'>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
       <input
         type='text'
         placeholder='Search for Chats'
         className='input input-bordered rounded-full w-full'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button
         type='submit'
